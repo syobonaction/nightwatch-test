b867b293028179a9e1c86b9011d94d6fdbbcd200
Charles Cole <charles.cole@grainger.com>
2019-09-04 16:10:49 -0500

    Added initial google advanced search tests.

---
+++ tests/google.js
@@ -0,0 +1,39 @@
+module.exports = {
+    '@tags': ['google'],
+    'Google Advanced Search: Vladimir Lenin': browser => {
+        // values
+        const querySearchWordsByAll = 'Vladimir Lenin';
+
+        // form selectors
+        const searchWordsByAllInputSelector = 'input[name="as_q"]';
+        const languageDropdownOpenerSelector = '#lr_button';
+        const languageDropdownValueSelector = '.goog-menuitem[value="lang_ru"]';
+        const lastUpdatedDropdownOpenerSelector = '#as_qdr_button';
+        const lastUpdatedDropdownValueSelector = '.goog-menuitem[value="m"]';
+        const submitButtonSelector = '.jfk-button[type="submit"]';
+
+        // search results selectors
+        const mainSearchSelector = `#searchform input[name="q"][value="${querySearchWordsByAll}"]`;
+
+        browser
+            .url('https://www.google.com/advanced_search')
+            .waitForElementVisible(searchWordsByAllInputSelector)
+            .setValue(searchWordsByAllInputSelector, querySearchWordsByAll)
+            .click(languageDropdownOpenerSelector)
+            .click(languageDropdownValueSelector)
+            .click(lastUpdatedDropdownOpenerSelector)
+            .click(lastUpdatedDropdownValueSelector)
+            .click(submitButtonSelector)
+
+            // Parameter assertions
+            browser
+                .assert.urlContains('as_q=Vladimir+Lenin', `Params: Query is "${querySearchWordsByAll}."`)
+                .assert.urlContains('lr=lang_ru', 'Params: Language is Russian.')
+                .assert.urlContains('as_qdr=m', 'Params: Time period is last month.');
+
+            // UI assertions
+            browser
+                .assert.visible(mainSearchSelector, `UI: "${querySearchWordsByAll}" is set in the query input.`)
+                .saveScreenshot('tests_output/google.png');
+    }
+}
--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'first_test_case': browser => {
+    'First test case': browser => {
         browser
             .url('https://east.paxsite.com')
             .waitForElementVisible('#countdown')
